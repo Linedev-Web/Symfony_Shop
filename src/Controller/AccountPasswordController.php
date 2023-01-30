e0d83b8a228d94165b8f7e50c7b96a0f24047d14
Latchere Yoan <latchere.y@gmail.com>
2023-01-30 19:14:57 +0100

    Create update password

---
+++ src/Controller/AccountPasswordController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountPasswordController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/account/password', name: 'account_password')]
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+        $notification = null;
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($hasher->isPasswordValid($user, $form->get('old_password')->getData())) {
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $hasher->hashPassword($user, $new_pwd);
+
+                $user->setPassword($password);
+                $this->entityManager->flush();
+                $notification = 'Votre mot de passe à bien été mise à jour';
+            } else {
+                $notification = 'Error';
+            }
+        }
+
+        return $this->render('account/password.html.twig', [
+            'form' => $form->createView(),
+            'notification' => $notification
+        ]);
+    }
+}
